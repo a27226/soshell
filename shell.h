4b84d949721750eacc70c81d152a0c3a90d9231b
a27226 <nelson.canhoto@live.com.pt>
2017-06-09 15:39:29 +0100

    Committing shell.h

--- shell.h
+++ shell.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <string.h>
@@ -8,12 +9,20 @@
 #include <pthread.h>
 #include <ctype.h>
 
+typedef struct
+{
+	int in, out, size;
+} myType;
+
 void parse(char *buf, char **args);
 
 void execute(char **args);
 
 int builtin (char **args);
 
+void lowlevelcopy(int in, int out, int size);
+
+void *wrapperCopy(void *args);
 /* constantes que podem tornar uteis*/
 
 #define BG 0
